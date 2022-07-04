87d4bb555d7e13f9016d35f83e4a134c7d2042f3
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-07-04 22:49:53 +0300

    ok

--- API/capital_finder.py
+++ API/capital_finder.py
@@ -1,41 +1,43 @@
+from http.client import REQUESTED_RANGE_NOT_SATISFIABLE
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        BASE_URL = "https://restcountries.com/v3.1"
-        s = self.path
-        url_components = parse.urlsplit(s)
+        path = self.path
+        url_components = parse.urlsplit(path)
         query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
+        print(query_string_list)
+        dictionary = dict(query_string_list)
+        url = "https://restcountries.com/v3.1/"
+        country = dictionary.get("country")
+        capital = dictionary.get("capital")
 
-        country = dic.get("country")
-        capital = dic.get("capital")
-
-        if country and capital:
-            message = "Stretch goal coming soon"
-        elif country:
-            url = f"{BASE_URL}/name/{country}"
-            r = requests.get(url)
-            data = r.json()
+        if capital:
+            response = requests.get(url + "capital/" + capital)
+            data = response.json()
             capitals = data[0]["capital"]
-            joined_capitals = " and ".join(capitals)
-            message = f"The capital of {country} is {joined_capitals}"
-        elif capital:
-            url = f"{BASE_URL}/capital/{capital}"
-            r = requests.get(url)
-            data = r.json()
-            message = str(data)
+            country_name = data[0]["name"]["common"]
+            print(capitals)
+            message = f"The capital of {country_name} is {capitals[0]}"
+
+        elif country:
+            response = requests.get(url + "name/" + country)
+            data = response.json()
+            capital_response = data[0]["capital"]
+
+            message = f"{capital_response[0]} is the capital of {country}"
 
         else:
-            message = "Supply a country or capital please"
+            message = "Give me a valid country please"
 
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
         self.wfile.write(message.encode())
-
         return
+
